dcd2c88b2d1e6da44fece5be5c2f6fb0e7ee2b3b
Yan Malashonak <malashonock@gmail.com>
2022-05-15 15:11:42 +0300

    feat: implement 05-merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,27 @@
+const { dir } = require('console');
+const fs = require('fs');
+const path = require('path');
+
+const SRC_FOLDER = 'styles';
+const DEST_FOLDER = 'project-dist';
+const DEST_FILE = 'bundle.css';
+
+const ws = fs.createWriteStream(path.join(__dirname, DEST_FOLDER, DEST_FILE));
+
+fs.readdir(
+  path.join(__dirname, SRC_FOLDER),
+  { withFileTypes: true },
+  (error, dirEntries) => {
+    if (error) throw error;
+
+    dirEntries
+      .filter((dirEntry) => dirEntry.isFile())
+      .filter((dirEntry) => dirEntry.name.endsWith('.css'))
+      .forEach((dirEntry) => {
+        const rs = fs.createReadStream(
+          path.join(__dirname, SRC_FOLDER, dirEntry.name)
+        );
+        rs.pipe(ws);
+      });
+  }
+);
