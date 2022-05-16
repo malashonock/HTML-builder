9113c4412502f50e07f787c0461ea8932bf4b7fc
Yan Malashonak <malashonock@gmail.com>
2022-05-16 22:07:48 +0300

    feat: implement copy assets function in 06-build-page task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,7 +1,9 @@
 const { createDistFolder } = require('./create-dist-folder');
 const { compileHtml } = require('./compile-html');
 const { compileCss } = require('./compile-css');
+const { copyAssets } = require('./copy-assets');
 
 createDistFolder();
 compileHtml();
 compileCss();
+copyAssets();
